d69dcbfec69da732cf66fd0285873cbddf8f55d2
会祈祷的蜗牛 <1975346264@qq.com>
2019-04-07 16:33:14 +0800

    Create stars.hpp

---
+++ Lab1_SolarSystem/stars.hpp
@@ -0,0 +1,48 @@
+//
+//  stars.hpp
+//  solarsystem
+//
+//
+#ifndef stars_hpp
+#define stars_hpp
+
+#include <GL/glut.h>
+
+class Star {
+public:
+    GLfloat radius;
+    GLfloat speed, selfSpeed;
+    GLfloat distance;
+    GLfloat rgbaColor[4];
+
+    Star* parentStar;
+
+    Star(GLfloat radius, GLfloat distance,
+         GLfloat speed,  GLfloat selfSpeed,
+         Star* parentStar);
+    void drawStar();
+    virtual void draw() { drawStar(); }
+    virtual void update(long timeSpan);
+protected:
+    GLfloat alphaSelf, alpha;
+};
+
+class Planet : public Star {
+public:
+    Planet(GLfloat radius, GLfloat distance,
+           GLfloat speed,  GLfloat selfSpeed,
+           Star* parentStar, GLfloat rgbColor[3]);
+    void drawPlanet();
+    virtual void draw() { drawPlanet(); drawStar(); }
+};
+
+class LightPlanet : public Planet {
+public:
+    LightPlanet(GLfloat Radius, GLfloat Distance,
+                GLfloat Speed,  GLfloat SelfSpeed,
+                Star* parentStar, GLfloat rgbColor[]);
+    void drawLight();
+    virtual void draw() { drawLight(); drawPlanet(); drawStar(); }
+};
+
+#endif /* star_hpp */
