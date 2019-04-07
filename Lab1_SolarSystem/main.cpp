c8ab953f944dec87b626979866297186f25bd79b
会祈祷的蜗牛 <1975346264@qq.com>
2019-04-07 16:31:38 +0800

    Update main.cpp

--- Lab1_SolarSystem/main.cpp
+++ Lab1_SolarSystem/main.cpp
@@ -1,35 +1,35 @@
 //
 //  main.cpp
 //  solarsystem
+//
 #include <GL/glut.h>
 #include "solarsystem.hpp"
 
+#define WINDOW_X_POS 50
+#define WINDOW_Y_POS 50
+#define WIDTH 700
+#define HEIGHT 700
+
 SolarSystem solarsystem;
 
-
 void onDisplay(void) {
-  solarsystem.onDisplay();
+    solarsystem.onDisplay();
 }
 void onUpdate(void) {
-  solarsystem.onUpdate();
+    solarsystem.onUpdate();
 }
-void onKeyboard(unsigned char key, int x,. int y) {
-  solarsystem.onKeyboard(key, x, y);
+void onKeyboard(unsigned char key, int x, int y) {
+    solarsystem.onKeyboard(key, x, y);
 }
 
-
-
-int main(int argc, char* argv[]) {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-  glutInitWindowPosition(WINDOW_X_POS, WINDOWS_Y_POS);
-  glutCreateWindow("SolarSysten at Wan Chen");
-	
-  glutDisplayFunc();
-  glutIdleFunc();
-  glutKeyboardFunc();
-  glutMainLoop();
-	
-  return 0;
-
+int main(int argc, char*  argv[]) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA |  GLUT_DOUBLE);
+    glutInitWindowPosition(WINDOW_X_POS, WINDOW_Y_POS);
+    glutCreateWindow("SolarSystem at Wan Chen");
+    glutDisplayFunc(onDisplay);
+    glutIdleFunc(onUpdate);
+    glutKeyboardFunc(onKeyboard);
+    glutMainLoop();
+    return 0;
 }
